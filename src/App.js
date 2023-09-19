5e9e5bd321e22a573bbcdf9e0f5cddb9249a2a22
Balaji g <balaji@rapidsoftworks.com>
2023-09-19 20:14:41 +0530

    MUI typography done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Typography variant='h3' component='p' align='center' gutterBottom={true} sx={{ backgroundColor: 'aliceblue', color: 'yellowgreen' }} >
+      Hi Bala....
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Beatae atque ea suscipit accusantium exercitationem et,
+      rerum dolorum molestias necessitatibus. Enim voluptas unde atque.
+      Sapiente commodi est nulla corrupti, nemo facilis.
+
+    </Typography>
+  );
+}
+
+export default App;
